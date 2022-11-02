d9bd9df77e0b6f3f760b87fff3e9c5599536bbad
Yari-Carelli <yaricarelli@hotmail.it>
2022-11-02 15:37:29 +0000

    Change default image

--- posts/models.py
+++ posts/models.py
@@ -29,7 +29,7 @@ class Post(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField(blank=True)
     image = models.ImageField(
-        upload_to='images/', default='../cld-sample-4', blank=True
+        upload_to='images/', default='../default_post_wdyur2', blank=True
     )
     image_filter = models.CharField(
         max_length=32, choices=image_filter_choices, default='normal'
